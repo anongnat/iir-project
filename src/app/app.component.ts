5a77c9efbaced833f085b58e9a7cb070f84bd802
antystang <anongnat_mahawang@cmu.ac.th>
2024-01-19 18:13:50 +0700

    iir-project : created new components and adjust styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component,ElementRef,HostListener,Inject } from '@angular/core';
+import { WINDOW } from './services/window.service';
+import { DOCUMENT } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'iirProjectApp';
+  constructor( @Inject(DOCUMENT) private document: Document,
+  @Inject(WINDOW) private window: Window,private elem: ElementRef){
+  }
+
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+    const offset = this.window.pageYOffset || this.document.documentElement.scrollTop || this.document.body.scrollTop || 0;
+    let elements = this.elem.nativeElement.querySelectorAll('.app-nav-bar');
+    if(offset > 200){
+      elements[0].classList.add('test');
+    }else{
+      elements[0].classList.remove('test');
+    }
+  }
 }
