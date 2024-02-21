95edd1b884aee83f177e5966f19ca39c652d5aa0
antystang <anongnat_mahawang@cmu.ac.th>
2024-02-21 19:54:27 +0700

    adjust css

--- src/app/lib/nav-bar/nav-bar.component.ts
+++ src/app/lib/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
@@ -6,7 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent {
+  @Input() from ='';
   displaySquare = true;
+  
 
   constructor() {
   }
